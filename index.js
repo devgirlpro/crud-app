9396a2f061bb43a70cd51f718349647eb9a2dbb8
devgirlpro <agalidari@gmail.com>
2024-04-12 13:58:55 +0200

    connect to database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 
@@ -8,5 +9,17 @@ app.listen(3001, () => {
 
 
 app.get('/',  (req, res) => {
-    res.send('Hello From Node API')
+    res.send('Hello From Node API server****')
+  });
+
+
+
+
+
+  mongoose.connect("mongodb+srv://agalidari:Azadehgalidari1992@backenddb.hmqycvv.mongodb.net/Node- API?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to dataBase!")
+  })
+  .catch(() => {
+    console.log("Connection faileed!")
   })
