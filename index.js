7332861231fa500340e395f0df66366b39eb1314
devgirlpro <agalidari@gmail.com>
2024-04-12 19:04:47 +0200

    restructure and create seprate folders and file for product route and product controller

--- index.js
+++ index.js
@@ -1,98 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require("./models/product.model.js");
 
+const productRoute = require("./routes/product.route.js");
 const app = express();
 
-//in post method, not allow to pass json through our node
-//that for have to use a moddleware
-app.use(express.json())
 
+//middlware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Hello From Node API server****');
-  });
-  
-
-//get all products => Product.find({})
-app.get("/api/products", async (req, res) => {
-    try {
-        const prodacts = await Product.find({})
-        res.status(200).json(prodacts)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-//get one products => Product.findById(id)
-app.get("/api/product/:id", async (req, res) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findById(id)
-
-        if(!product) {
-            return res.status(404).json({message: "Product not found"})
-        }  
-
-        res.status(200).json(product)
-
-    } catch(error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-//update product => app.put => Product.findByIdAndUpdate(id, req.body)
-app.put("/api/product/:id", async(req, res) => {
-    try {
-        const  { id } = req.params;
-        const product = await Product.findById(id)
-
-        if(!product) {
-            return res.status(404).json({message: "Product not found"})
-        }
-
-        const updatedProduct = await Product.findByIdAndUpdate(id, req.body);
-        res.status(200).json(updatedProduct)
-
-    } catch(error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-
-
-//delete a product
-app.delete("/api/product/:id", async (req, res) => {
-    try {
-        const  { id } = req.params;
-        const product = await Product.findById(id)
-
-        if(!product) {
-            return res.status(404).json({message: "Product not found"})
-        }
-
-        const deletedProduct = await Product.findByIdAndDelete(id);
-        res.status(200).json({message: "product delted successfully"})
-
-    } catch(error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-
-//post to save product in our database => Product.create(req.body)
-app.post('/api/products', async (req, res) => {
-    try{
-       const product =  await Product.create(req.body);
-       res.status(200).json(product)
-    } catch(error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
+// routes
+app.use("/api/products", productRoute);
 
 mongoose
   .connect(
@@ -101,13 +19,9 @@ mongoose
   .then(() => {
     console.log('Connected to dataBase!');
     app.listen(3001, () => {
-        console.log('Server is running on port 3001');
-      });
+      console.log('Server is running on port 3001');
+    });
   })
   .catch(() => {
     console.log('Connection faileed!');
   });
-
-
-
-
