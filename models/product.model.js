db4993503a841e8702aded41136789552a34d001
devgirlpro <agalidari@gmail.com>
2024-04-12 14:30:47 +0200

    create modelSchema

---
+++ models/product.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      require: [true, 'Please enter the name'],
+      type: String,
+    },
+    quantiti: {
+      require: true,
+      type: Number,
+      default: 0,
+    },
+    price: {
+      require: true,
+      type: Number,
+      default: 0,
+    },
+    image: {
+      type: String,
+      require: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
