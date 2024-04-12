4cc2e86005316c325878336c5dd8cb414967ce5b
devgirlpro <agalidari@gmail.com>
2024-04-12 14:03:32 +0200

    first connect to database then listen to server

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
-const app = express()
+const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello From Node API server****');
+  });
+  
 
-app.listen(3001, () => {
-    console.log("Server is running on port 3001")
-})
-
-
-app.get('/',  (req, res) => {
-    res.send('Hello From Node API server****')
+mongoose
+  .connect(
+    'mongodb+srv://agalidari:Azadehgalidari1992@backenddb.hmqycvv.mongodb.net/Node- API?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to dataBase!');
+    app.listen(3001, () => {
+        console.log('Server is running on port 3001');
+      });
+  })
+  .catch(() => {
+    console.log('Connection faileed!');
   });
 
 
 
 
-
-  mongoose.connect("mongodb+srv://agalidari:Azadehgalidari1992@backenddb.hmqycvv.mongodb.net/Node- API?retryWrites=true&w=majority")
-  .then(() => {
-    console.log("Connected to dataBase!")
-  })
-  .catch(() => {
-    console.log("Connection faileed!")
-  })
